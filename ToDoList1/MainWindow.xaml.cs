0ebb65d804880c043dfcd2092dded4d07716f746
martsev.ad <martsev.96@gmail.com>
2024-11-22 10:50:30 +0700

    Добавьте файлы проекта.

---
+++ ToDoList1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToDoList1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Dictionary<string, bool> _TaskList;
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            _TaskList = new Dictionary<string, bool>();
+            InitializeComponent();
+
+            foreach (var task in _TaskList)
+            {
+                lstBoxTasks.Items.Add(task.Key);
+            }
+
+        }
+
+
+        private void btnAddTask_Click(object sender, RoutedEventArgs e)
+        {
+            TaskEditor taskEditor = new TaskEditor(_TaskList);
+            taskEditor.ShowDialog();
+            lstBoxTasks.Items.Add(_TaskList.Last().Key);
+        }
+
+    }
+}
---
+++ ToDoList1/TaskEditor.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ToDoList1
+{
+    /// <summary>
+    /// Логика взаимодействия для TaskEditor.xaml
+    /// </summary>
+    public partial class TaskEditor : Window
+    {
+
+        Dictionary<string, bool> _taskList;
+        public TaskEditor(Dictionary<string, bool> value)
+        {
+            InitializeComponent();
+            _taskList = value;
+        }
+
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+            _taskList.Add(new TextRange(richTxtBoxTask.Document.ContentStart, richTxtBoxTask.Document.ContentEnd).Text, false);
+            this.Close();
+            //запихнуть запись в список уже имеющихся дел
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+       
+    }
+}
